79c29b5260bfd55d5ab1a1927736992f7bfc071c
KomalJaiswal10 <100574146+KomalJaiswal10@users.noreply.github.com>
2022-02-28 16:57:20 +0530

    Add files via upload

---
+++ project-random password.py
@@ -0,0 +1,30 @@
+import string
+import random
+
+'''scaps = string.ascii_uppercase
+slow = string.ascii_lowercase
+sdig = string.digits
+spunct = string.punctuation
+
+pcaps = random.choice(scaps)
+plow = random.choice(slow)
+pdig = random.choice(sdig)
+ppunct = random.choice(spunct)
+
+print("Your Password : ", pcaps+plow+ppunct+pdig)'''
+
+scaps = string.ascii_uppercase
+slow = string.ascii_lowercase
+sdig = string.digits
+spunct = ['@','$','_','&']
+
+pcaps = random.choice(scaps)
+plow = random.choice(slow)
+pdig = random.choice(sdig)
+ppunct = random.choice(spunct)
+
+print("Your Password : ", pcaps+plow+ppunct+pdig)
+
+
+
+
